5a75a4970fe1d423cedb7ea673173b2552304142
Austin Rippee <austinrippee@gmail.com>
2025-07-29 23:44:15 -0400

    Getting files up to date

--- trainingpaces/trainingpaces/TrainingPaces.java
+++ trainingpaces/trainingpaces/TrainingPaces.java
@@ -1,8 +1,19 @@
 package trainingpaces;
 
 public class TrainingPaces {
-    public static void main(String[] args)
-    {
-        System.out.println("Running Training Paces...");
+    public static void main(String[] args) {
+        //connect to database
+
+        //have user enter distance
+        //have user enter time
+
+        //use time to create the query to filter out the VDOT race table
+
+        //either have a separate query or simply join on the ID from the VDOT race table
+        //and combine to get the training paces
+
+        //print out the training paces based on that as well as the VDOT score
+
+        //BONUS: take this VDOT score and bring in comparable distances from PaceConvert
     }
 }
